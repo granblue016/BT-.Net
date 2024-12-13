5702ba55f7740432491a29f1305cc638f1e8c7d9
granblue016 <nguyenhoanganhkha1102@gmail.com>
2024-12-13 10:52:37 +0700

    End of Section 15

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,4 +1,4 @@
-using API.DTOs;
+﻿using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using AutoMapper;
@@ -17,5 +17,10 @@ public class AutoMapperProfiles : Profile
         CreateMap<MemberUpdateDto, AppUser>();
         CreateMap<RegisterDto, AppUser>();
         CreateMap<string, DateOnly>().ConvertUsing(s => DateOnly.Parse(s));
-    }
+        CreateMap<Message, MessageDto>()
+            .ForMember(d => d.SenderPhotoUrl, 
+                o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain)!.Url))
+            .ForMember(d => d.RecipientPhotoUrl, 
+                o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+    }       
 }
